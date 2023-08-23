6117da7151446bf426f82e7498b00061c8be6a9d
Deepankar Siddharth <65945129+Deepankar-Siddharth@users.noreply.github.com>
2023-08-23 23:14:19 +0530

    Update autorun.ps1

--- ps/autorun.ps1
+++ ps/autorun.ps1
@@ -1,2 +1,1 @@
-Start-Process -FilePath www.youtube.com/watch?v=KjGRUox8KlE&list=PLBZrNHl2zz_aN7M0ErZGZ7slnQrdRlapm
 Start-Process -FilePath Chrome -ArgumentList www.youtube.com/watch?v=KjGRUox8KlE&list=PLBZrNHl2zz_aN7M0ErZGZ7slnQrdRlapm
