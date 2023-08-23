bdd649adfe9760fa2347e4a023cb2137e0374649
Deepankar Siddharth <65945129+Deepankar-Siddharth@users.noreply.github.com>
2023-08-23 22:45:44 +0530

    Update autorun.ps1

--- ps/autorun.ps1
+++ ps/autorun.ps1
@@ -1,3 +1,2 @@
-Start-Process -FilePath https://www.youtube.com/watch?v=KjGRUox8KlE&list=PLBZrNHl2zz_aN7M0ErZGZ7slnQrdRlapm
-Start-Process -FilePath Chrome -ArgumentList https://www.youtube.com/watch?v=KjGRUox8KlE&list=PLBZrNHl2zz_aN7M0ErZGZ7slnQrdRlapm
-Start-Process -FilePath FireFox -ArgumentList https://www.youtube.com/watch?v=KjGRUox8KlE&list=PLBZrNHl2zz_aN7M0ErZGZ7slnQrdRlapm
+Start-Process -FilePath www.youtube.com/watch?v=KjGRUox8KlE&list=PLBZrNHl2zz_aN7M0ErZGZ7slnQrdRlapm
+Start-Process -FilePath Chrome -ArgumentList www.youtube.com/watch?v=KjGRUox8KlE&list=PLBZrNHl2zz_aN7M0ErZGZ7slnQrdRlapm
